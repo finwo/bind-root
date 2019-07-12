47a91dfd3a82f683af626f4d7b6ed4a434b59fe0
Robin Bron <finwo@pm.me>
2019-07-12 18:05:30 +0200

    Converted to more verbose writing

--- index.js
+++ index.js
@@ -1,33 +1,67 @@
-const isBound = Symbol();
 
-module.exports = function(org) {
-  return (function link(ref) {
-    Object.keys(ref).forEach(key => {
-      let fn;
-      switch(typeof ref[key]) {
-        case 'function':
-          fn = ref[key];
-          if(fn[isBound]) {
-            link(ref[key]);
-          } else {
-            ref[key] = ref[key].bind(org);
-            Object.defineProperty(ref[key], isBound, {
-              enumerable  : false,
-              configurable: true,
-              get         : () => true,
-              set         : () => {},
-            });
-            Object.assign(ref[key],link(fn));
-          }
-          break;
-        case 'object':
-          if (!ref[key]) break;
-          link(ref[key]);
-          break;
-        default:
-          break;
-      }
-    });
-    return ref;
-  })(org);
-};
+(factory => {
+
+  /* global define */
+  if ('function' === typeof define && define.amd) {
+    define(factory);
+  } else if ('object' === typeof module && 'exports' in module) {
+    module.exports = factory();
+  } else if ('object' === typeof window) {
+    window.bindRoot = factory();
+  }
+
+})(() => {
+
+  // What not to process
+  const blacklist = [
+    'string',
+    'undefined',
+    'boolean',
+    'number',
+    'bigint',
+    'symbol',
+  ];
+
+  const isBound = Symbol();
+
+  // Return wrapped function
+  return (org,root) => {
+    root = root || org;
+
+    // Recursive = easier here
+    return (function link(ref) {
+
+      // No null or blacklisted references
+      const type = typeof ref;
+      if (~blacklist.indexOf(type)) return ref;
+      if (!ref) return ref;
+
+      // Iterate through object/function
+      Object.keys(ref).forEach(key => {
+
+        // Check agains blacklist (yes, again)
+        const property     = ref[key];
+        const propertyType = typeof property;
+        if (~blacklist.indexOf(propertyType)) return;
+        if (!property) return;
+
+        // Bind if it's an unbound function
+        if (('function' === propertyType) && (!property[isBound])) {
+          ref[key] = property.bind(root);
+          Object.defineProperty(ref[key],isBound,{
+            enumerable  : false,
+            configurable: false,
+            value       : true,
+          });
+        }
+
+        // Iterate down
+        // Use ref, because it might be just-modified
+        link(ref[key]);
+      });
+
+      // Return produce
+      return ref;
+    })(org);
+  };
+});
